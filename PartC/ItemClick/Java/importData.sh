8b648376d34ea321f9eaae0c48e9300be7d5f71d
Michael Massey <michael@michaelmassey.com>
2018-09-22 20:28:24 +0000

    Fix PartC - ItemClick scripts - Again

--- PartC/ItemClick/Java/importData.sh
+++ PartC/ItemClick/Java/importData.sh
@@ -19,8 +19,8 @@ wget https://s3-us-west-2.amazonaws.com/cs5253-project1/${DOWNLOAD_FILE}
 
 # Copy input into input dir
 echo ----------------------------------------------------------
-echo Copying input data into input dir...
-cp ./${DOWNLOAD_FILE} ./input
+echo Moving input data into input dir...
+mv ./${DOWNLOAD_FILE} ./input
 
 echo ----------------------------------------------------------
 echo Finished
