32e5d2f42d29d16a7ade1aefcd2102361cd21784
Michael Massey <michael@michaelmassey.com>
2018-09-22 20:17:37 +0000

    Updated PartC - ItemClick scripts

--- PartC/ItemClick/Java/importData.sh
+++ PartC/ItemClick/Java/importData.sh
@@ -2,15 +2,25 @@
 
 DOWNLOAD_FILE=clicks.txt
 
+# Remove old input data
+echo ----------------------------------------------------------
+echo Removing old input data
+rm -r ./input
+
 # Create directories
 echo ----------------------------------------------------------
 echo Creating input directory...
-mkdir input
+mkdir ./input
 
 # Download input file
 echo ----------------------------------------------------------
 echo Retrieving input file...
-wget https://s3-us-west-2.amazonaws.com/cs5253-project1/${INPUT_FILE}
+wget https://s3-us-west-2.amazonaws.com/cs5253-project1/${DOWNLOAD_FILE}
+
+# Copy input into input dir
+echo ----------------------------------------------------------
+echo Copying input data into input dir...
+cp ./${DOWNLOAD_FILE} ./input
 
 echo ----------------------------------------------------------
 echo Finished
