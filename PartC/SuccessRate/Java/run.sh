0107578cfc69fc1b84e372d318df72ef6ccaedfa
cxlisolaf <doramifa@gmail.com>
2018-09-25 19:59:46 -0600

    add run.sh

---
+++ PartC/SuccessRate/Java/run.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+INPUT_FILE1=clicks.txt
+INPUT_FILE2 = buys.txt
+JAR_FILE=SuccessRate.jar
+CLASS=SuccessRate
+LOCAL_INPUT=./input
+LOCAL_OUTPUT=./output
+HDFS_INPUT=/input
+HDFS_OUTPUT=/output
+
+# Remove Existing Output
+echo ----------------------------------------------------------
+echo Remove existing output files...
+hadoop fs -rm -r ${HDFS_OUTPUT}
+rm ${LOCAL_OUTPUT}
+
+# Create directories
+echo ----------------------------------------------------------
+echo Creating directories...
+hadoop fs -mkdir ${HDFS_INPUT}
+
+# Move data file to hdfs
+echo ----------------------------------------------------------
+echo Copying input file to hdfs...
+hadoop fs -copyFromLocal ${LOCAL_INPUT}/${INPUT_FILE1} ${HDFS_INPUT}
+hadoop fs -copyFromLocal ${LOCAL_INPUT}/${INPUT_FILE2} ${HDFS_INPUT}
+# Run Hadoop job
+echo ----------------------------------------------------------
+echo Running Hadoop job...
+hadoop jar ./${JAR_FILE} ${CLASS} ${HDFS_INPUT} ${HDFS_OUTPUT}
+
+# Move output data to local file system
+echo ----------------------------------------------------------
+echo Copying output files...
+hadoop fs -copyToLocal ${HDFS_OUTPUT}/p* ${LOCAL_OUTPUT}
+
+# Cleanup hdfs files
+echo ----------------------------------------------------------
+echo Cleaning up hdfs...
+hadoop fs -rm -r ${HDFS_INPUT}
+hadoop fs -rm -r ${HDFS_OUTPUT}
+
+# Verifying results
+echo ----------------------------------------------------------
+echo Verifying results...
+FILE_COUNT=`ls -1 ${LOCAL_OUTPUT} | wc -l`
+LINE_COUNT=`cat ${LOCAL_OUTPUT} | wc -l`
+echo Output File Count: ${FILE_COUNT}
+echo Output Line Count: ${LINE_COUNT}
+
+echo ----------------------------------------------------------
+echo Finished
+echo ----------------------------------------------------------
--- PartC/SuccessRate/Java/src/main/java/SuccessRateReducer.java
+++ PartC/SuccessRate/Java/src/main/java/SuccessRateReducer.java
@@ -10,7 +10,7 @@ import org.apache.hadoop.io.FloatWritable;
 
 
 
-public class SuccessRateReducer extends Reducer<Text, BeanSetup, Text, String> {
+public class SuccessRateReducer extends Reducer<Text, BeanSetup, Text, FloatWritable> {
 
 	// Set number of desired results (top n)git
 	private static final int NUM_RESULTS = 10;
@@ -54,7 +54,7 @@ public class SuccessRateReducer extends Reducer<Text, BeanSetup, Text, String> {
 
 			// Iterate results in descending order and write to context
 			for (Entry<Float, String> entry : resultMap.descendingMap().entrySet()) {
-				context.write(new Text(entry.getValue()), new String(String.format("%0.2f", entry.getKey())));
+				context.write(new Text(entry.getValue()), new FloatWritable(entry.getKey()));
 			}
 	}
 
